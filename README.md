498863cd49a88f2da9ad37be70ce3a0e0bd9514d
rajvardhan19 <82277023+rajvardhan19@users.noreply.github.com>
2021-12-05 12:29:10 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Math_Wiz
+
+This script is mainly writen for teachers who have to form more and more questions of the same type for their students. The user just has to specify the number of questions, type of operations and the number of values on which that operatin has to be performed.This program will automatically generate random questions with the parameters specified by the user. The answers will also be provided so there is no ned for manual calculations. 
+The screenshots of the same are added to the images.pdf file.
---
+++ math_wiz.py
@@ -0,0 +1,37 @@
+# Question Builder
+# To build random question for Adhyapan
+
+# List of Numeric Problems
+
+# Importing function
+import random
+import math
+
+# n stands for number of questions
+n = int(input("Please enter the number of questions: "))
+
+# Select the operation: Addition, substraction, ......
+
+a = int(input("Please select the Question type: \n Press 1 for Addition  \n Press 2 for Multiplication  \n Enter your choice here:  " ))
+digits = int(input("The operation should be performed on how many numbers: "))
+
+x = 0
+while (x < n):
+   list = []
+   i = 0
+   x = x + 1
+   while (i < digits):
+      r = random.choice(range(11,20000))
+      list.append(r)
+      if len(list) == digits:
+         if a == 1:
+           print("Solve:")
+           print(*list, sep = " + ")
+           print("Answer = ", (sum(list)))
+         elif a == 2:
+            print("Solve:")
+            print(*list, sep = " * ")
+            print("Answer = ", math.prod(list)) 
+         else:
+            print("Operation Invalid!!!")
+      i = i + 1
